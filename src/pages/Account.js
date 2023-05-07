b64f2c1a6119bb8fe3cabaf1562c29d0b90d4e53
SolveigRebnord <91594496+SolveigRebnord@users.noreply.github.com>
2023-05-08 00:23:21 +0200

    First part ready. Need to find solution to menu and scroll

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -6,10 +6,14 @@ import { getFilteredBookings } from "../store/modules/BookingSlice";
 import FadeIn from "react-fade-in/lib/FadeIn";
 import { useState } from "react";
 import React from "react";
+import Menu from "../components/Menu";
 
 const Account = () => {
 
     const dispatch = useDispatch();
+
+
+
 
     const { user: currentUser } = useSelector((state) => state.auth);
     const { singleProfile } = useSelector((state) => state.profiles);
@@ -18,7 +22,6 @@ const Account = () => {
     if (!currentUser) {
         window.location.replace('/login')
     }
-
 
     let name = currentUser.name;
 
@@ -30,9 +33,6 @@ const Account = () => {
 
     }
   }, [dispatch, name]);
-
-
-
 
 
 
@@ -66,66 +66,65 @@ const Account = () => {
       }
  
 
-
+    
 
 
 
   return (
-    <><FadeIn transitionDuration={1000}>
-      <section className="pt-32 mb-96">
-      <FadeIn delay={200} >
-        <div className="bg-white p-12 py-24 w-fit ml-16 shadow-xl drop-shadow-md relative">
-          {name ? <h1 className="">Hi {name}!</h1> : <h1>Hi there!</h1>}
-  
-          {singleProfile && 
-          <div>
-          
-              <p>{singleProfile.email}</p>
-              <p>{singleProfile.name}</p>
-     
-      
+    <>
+         <section className="relative z-0 mb-96 ">
+            <img src="/palmtree.jpg" className="h-[300px] min-w-full object-cover object-left"></img>
+         
+          </section>
+    
+          {singleProfile && <>
 
-              <img
+      <section className="absolute top-52 left-1/2 -translate-x-1/2  w-1/2 flex flex-row items-center justify-center gap-32">
+      <FadeIn delay={400} ><Menu /></FadeIn>
+      <FadeIn delay={200} >
+        <div className="bg-white flex flex-col gap-4 px-10 py-12 w-fit shadow-xl drop-shadow-md relative">
+          <div className="text-right ">
+            <button className="absolute top-4 right-4 "><img src="/gear.svg" /></button>
+          </div>
+          <FadeIn transitionDuration={1000}>
+          <div className="flex flex-row gap-8 items-end">
+            {name ? <h1 className="font-semibold text-xl"><span className="text-sm">Hi,</span> {name}!</h1> : <h1>Hi there!</h1>}
+            <img
                 src={singleProfile.avatar}
                 alt={singleProfile.name}
-                className="h-32 w-32 object-contain object-center"/>
-            
-        
-
-          </div>}
-          <ul className="absolute top-1/3 -right-[78%] flex flex-col gap-8">
-          <FadeIn delay={600}>
-            <li 
-            className="bg-[#004E56] rounded-md text-white font-semibold  w-full h-16 drop-shadow-md px-24 flex items-center justify-center hover:-translate-x-2 hover:scale-105 transition ease-in duration-100 delay-75 cursor-pointer">
-            Upcoming bookings</li>
-            </FadeIn>
-          <FadeIn delay={700}>
-            <li 
-            className="bg-[#004E56] rounded-md text-white font-semibold  w-full h-16 drop-shadow-md px-24 flex items-center justify-center hover:-translate-x-2 hover:scale-105 transition ease-in duration-100 delay-75 cursor-pointer">                
-            Upcoming bookings</li>
-                </FadeIn>              
-          <FadeIn delay={800}>
-            <li 
-            className="bg-[#004E56] rounded-md text-white font-semibold  w-full h-16 drop-shadow-md px-24 flex items-center justify-center hover:-translate-x-2 hover:scale-105 transition ease-in duration-100 delay-75 cursor-pointer">                
-            Upcoming bookings</li>
-                </FadeIn>
-
-          </ul>
+                className="h-24 w-24 object-cover object-center rounded-full"/>
+          </div>
+          </FadeIn>
+          <div className="flex flex-col gap-4">
+            <hr className=" bg-gray-300 shadow-none border-none h-[1px] mt-10" />
+            <span className="flex flex-col items-end text-sm">
+              <p className="text-xs text-gray-500 uppercase unde">Email</p>
+              <p className="underline">{singleProfile.email}</p>
+            </span>
+            <hr className=" bg-gray-300 shadow-none border-none h-[1px]" />
+            <span className="flex flex-row justify-end text-sm gap-2">
+              <p className="text-sm">Venuemanager</p>
+              <span className="text-base font-semibold text-black">
+                {singleProfile.venueManager ? <span>&#10003;</span> : <span>&#215;</span>}
+                </span>
+            </span>
+            <hr className=" bg-gray-300 shadow-none border-none h-[1px]" />
+          </div>
+    
         </div>
         </FadeIn>
       </section>
-      </FadeIn>
-      <div className="bg-poolFloor h-screen absolute w-1/2 -z-10 top-0 object-cover bg-no-repeat"></div>
 
-<section className="flex flex-col gap-20">
+
+<section className="flex flex-col gap-20" >
       <FadeInSection >
   <section className="bg-gray-300 h-96 w-full">
     <h2>Upcoming Bookings</h2>
-    {singleProfile &&         <ul className="flex flex-row gap-4 my-2 text-sm font-light lg:w-2/3 lg:mx-auto">
+     <ul className="flex flex-row gap-4 my-2 text-sm font-light lg:w-2/3 lg:mx-auto">
                 {singleProfile.bookings.map((booking) => (
-                  <li key={booking.name}>{booking.id}</li>
+                  <li key={booking.id}>{booking.id}</li>
                 ))}
-              </ul>}
+              </ul>
     
   </section>
 </FadeInSection>
@@ -133,18 +132,20 @@ const Account = () => {
 <FadeInSection >
   <section className="bg-gray-300 h-96 w-full">
     <h2>Your Venues</h2>
-    {singleProfile &&                  <ul className="flex flex-row gap-4 my-2 text-sm font-light lg:w-2/3 lg:mx-auto">
+    <ul className="flex flex-row gap-4 my-2 text-sm font-light lg:w-2/3 lg:mx-auto">
                 {singleProfile.venues.map((venue) => (
                   <li key={venue.name}>{venue.name}</li>
                 ))}
               </ul>
-}
+
     
   </section>
 </FadeInSection>
-</section>
+</section></>}
     </>
   );
 };
 
 export default Account;
+
+//bg-[#004E56] rounded-md text-white font-semibold  w-full h-16 drop-shadow-md px-24 flex items-center justify-center hover:-translate-x-2 hover:scale-105 transition ease-in duration-100 delay-75 cursor-pointer
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,14 @@ import VenueSlice from "./modules/VenueSlice";
 import BookingSlice from "./modules/BookingSlice";
 import AuthSlice from "./modules/AuthSlice";
 import ProfilesSlice from "./modules/ProfilesSlice";
+import MenuSlice from "./modules/MenuSlice";
 
 const reducer = combineReducers({
     venues: VenueSlice,
     bookings: BookingSlice,
     auth: AuthSlice,
-    profiles: ProfilesSlice
+    profiles: ProfilesSlice,
+    menu: MenuSlice
 });
 const index = configureStore({
   reducer,
